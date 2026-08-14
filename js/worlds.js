f9be7246629c4b8a3f953624cec928940adc9eef
falmonmickael260-commits <falmonmickael260@gmail.com>
2026-08-14 20:28:25 +0200

    Create worlds.js

---
+++ js/worlds.js
@@ -0,0 +1,9 @@
+export const WORLDS=[
+{icon:"🌳",name:"Forêt enchantée",door:"🌳",color:"forest"},
+{icon:"🏰",name:"Château magique",door:"🏰",color:"castle"},
+{icon:"🌊",name:"Monde sous-marin",door:"🌊",color:"ocean"},
+{icon:"🚀",name:"Planète des étoiles",door:"🚀",color:"space"},
+{icon:"🦖",name:"Vallée des dinosaures",door:"🦖",color:"dino"},
+{icon:"🏴‍☠️",name:"Île au trésor",door:"🏴‍☠️",color:"pirate"},
+{icon:"🌈",name:"Royaume magique",door:"🌈",color:"rainbow"}
+];
